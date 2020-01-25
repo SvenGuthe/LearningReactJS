f0575ec1436853950e348c86b3d72e087fd7fa1d
SvenGuthe <sven_guthe@web.de>
2020-01-25 13:19:57 +0100

    3.44 Using Hooks for functional Style

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends Component {
-  render() {
+const app = (props) => {
+
+  const [ personsState, setPersonsState ] = useState({
+    persons: [
+      {
+        name: 'Max',
+        age: 28
+      },
+      {
+        name: 'Manu',
+        age: 29
+      },
+      {
+        name: 'Stephanie',
+        age: 26
+      }
+    ],
+    otherState: 'some other values'
+  })
+
+  console.log(personsState);
+
+/*   state = {
+    persons: [
+      {
+        name: 'Max',
+        age: 28
+      },
+      {
+        name: 'Manu',
+        age: 29
+      },
+      {
+        name: 'Stephanie',
+        age: 26
+      }
+    ],
+    otherState: 'some other values'
+  } */
+
+  const switchNameHandler = () => {
+    /* console.log(this.state); */
+    setPersonsState({
+      persons: [
+        {
+          name: 'Maximilian',
+          age: 28
+        },
+        {
+          name: 'Manu',
+          age: 29
+        },
+        {
+          name: 'Stephanie',
+          age: 27
+        }
+      ],
+      otherState: personsState.otherState
+    });
+  }
+
+  // render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
-  }
+    /* return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App')); */
+  // }
+
 }
 
-export default App;
+/* export default App; */
+export default app;
