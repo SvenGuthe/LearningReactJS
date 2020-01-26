3e8db5da09e1993e11683e7c8394e85419c2c605
SvenGuthe <sven_guthe@web.de>
2020-01-26 14:20:46 +0100

    4.54 Condition in the JavaScript-Way

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -79,24 +79,33 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = <div>
+                  <Person 
+                    name={this.state.persons[0].name} 
+                    age={this.state.persons[0].age} />
+                  <Person 
+                    name={this.state.persons[1].name} 
+                    age={this.state.persons[1].age}
+                    click={this.switchNameHandler.bind(this, 'Max!')}
+                    changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+                  <Person 
+                    name={this.state.persons[2].name} 
+                    age={this.state.persons[2].age}/>
+                </div>
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <button style={style} onClick={this.togglePersonsHandler}>Toogle Persons</button>
-        { this.state.showPersons === true ?
-            <div>
-              <Person 
-                name={this.state.persons[0].name} 
-                age={this.state.persons[0].age} />
-              <Person 
-                name={this.state.persons[1].name} 
-                age={this.state.persons[1].age}
-                click={this.switchNameHandler.bind(this, 'Max!')}
-                changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-              <Person 
-                name={this.state.persons[2].name} 
-                age={this.state.persons[2].age}/>
-            </div> : null}
+        { 
+        // this.state.showPersons === true ?
+            persons
+        // : null
+        }
       </div>
     );
     /* return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App')); */
