98a62b698801fd1e0aea931ff3f05743e618c49f
SvenGuthe <sven_guthe@web.de>
2020-02-09 17:10:32 +0100

    7.106 Another Form of HOCs

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from './../hoc/WithClass'
+import withClass from '../hoc/withClass'
 
 class App extends Component {
 
@@ -85,7 +85,7 @@ class App extends Component {
     }
 
     return (
-      <WithClass classes={classes.App}>
+      <Fragment>
         <button onClick={() => {
           this.setState({
             showCockpit: !this.state.showCockpit
@@ -93,10 +93,10 @@ class App extends Component {
         }}>Toggle Cockpit</button>
         {this.state.showCockpit ? <Cockpit title={this.props.appTitle} personsLength={this.state.persons.length} showPersons={this.state.showPersons} clicked={this.togglePersonsHandler}></Cockpit> : null}
         {persons}
-      </WithClass>
+      </Fragment>
     );
   }
 
 }
 
-export default App;
+export default withClass(App, classes.App);
