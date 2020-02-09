8e041917bae855d10e435b4f0855c0de1169aa13
SvenGuthe <sven_guthe@web.de>
2020-02-09 17:04:40 +0100

    7.105 Higher Order Components (HOC)

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from './../hoc/WithClass'
 
 class App extends Component {
 
@@ -84,7 +85,7 @@ class App extends Component {
     }
 
     return (
-      <div className={classes.App}>
+      <WithClass classes={classes.App}>
         <button onClick={() => {
           this.setState({
             showCockpit: !this.state.showCockpit
@@ -92,7 +93,7 @@ class App extends Component {
         }}>Toggle Cockpit</button>
         {this.state.showCockpit ? <Cockpit title={this.props.appTitle} personsLength={this.state.persons.length} showPersons={this.state.showPersons} clicked={this.togglePersonsHandler}></Cockpit> : null}
         {persons}
-      </div>
+      </WithClass>
     );
   }
 
