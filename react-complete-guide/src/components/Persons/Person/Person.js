df7d4516c3cd6a1d7dc639ebd7f2602e89010bcd
SvenGuthe <sven_guthe@web.de>
2020-02-09 17:16:47 +0100

    7.107 Passing Unknows Props

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import classes from './Person.css';
+import withClass from './../../../hoc/withClass';
 
 class Person extends Component {
 
@@ -15,4 +16,4 @@ class Person extends Component {
     };
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
